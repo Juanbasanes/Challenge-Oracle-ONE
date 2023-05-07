9840b59b499633e8bf39af7fe5c1c9b1d5e7b1ae
Juanbasanes <jbcantoni@outlook.com>
2023-05-07 09:51:10 +0800

    seccion encriptado

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function encriptar() {
-    // traer el texto de entrada ingresado
-    const texto = document.getElementById("entrada").value;
+
+    //Obtener el texto de entrada
+    let entrada = document.getElementById("entrada").value;
+    //Limpiar variable resultado
+    let resultado = "";
   
-    // Chequear que el texto no tenga mayusculas ni acentos
-    const expReg = /^[a-zñ]+$/;
-    if (!expReg.test(texto)) {
-      alert("El texto debe estar compuesto solo por letras minúsculas y sin acentos");
+    //Logica de entrada, solo se permiten letras minúsculas sin acentos o caracteres especiales
+    if (/[^a-z]/.test(entrada)) {
+      document.getElementById("no-mensaje").style.display = "block";
+      document.getElementById("no-mensaje").textContent =
+        "Solo se permiten letras minúsculas sin acentos y sin caracteres especiales";
+      document.getElementById("resultado").value = "";
       return;
     }
+  
+    //Logica de encriptado, patron de remplazo
+    resultado = entrada
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
 
+    //Salida del encriptado
+    document.getElementById("resultado").value = resultado;
+    document.getElementById("no-mensaje").style.display = "none";
+  }
   
