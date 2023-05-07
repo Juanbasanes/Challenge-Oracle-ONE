ef05412accf15c753130a2f628362a6620175f54
Juanbasanes <jbcantoni@outlook.com>
2023-05-07 10:19:47 +0800

    funcion desencriptado

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ function encriptar() {
     let entrada = document.getElementById("entrada").value;
     //Limpiar variable resultado
     let resultado = "";
+
+    // Validar si se ingresó un mensaje
+    if (entrada === "") {
+        document.getElementById("no-mensaje").style.display = "block";
+        document.getElementById("no-mensaje").textContent = "ATENCION: No se ha encontrado ningun texto a procesar, porfavor ingrese un mensaje en minisculas y sin caracteres especiales!";
+        document.getElementById("resultado").value = "";
+        return;
+    }
   
     //Logica de entrada, solo se permiten letras minúsculas sin acentos o caracteres especiales
     if (/[^a-z]/.test(entrada)) {
       document.getElementById("no-mensaje").style.display = "block";
       document.getElementById("no-mensaje").textContent =
-        "Solo se permiten letras minúsculas sin acentos y sin caracteres especiales";
+        "ATENCION: Solo se permiten letras minúsculas sin acentos y sin caracteres especiales!";
       document.getElementById("resultado").value = "";
       return;
     }
-  
+    
+    
     //Logica de encriptado, patron de remplazo
     resultado = entrada
       .replace(/e/g, "enter")
@@ -25,4 +34,44 @@ function encriptar() {
     //Salida del encriptado
     document.getElementById("resultado").value = resultado;
     document.getElementById("no-mensaje").style.display = "none";
+
+}
+
+function desencriptar() {
+
+    //Obtener el texto de entrada
+    let entrada = document.getElementById("entrada").value;
+    //Limpiar variable resultado
+    let resultado = "";
+
+    // Validar si se ingresó un mensaje
+    if (entrada === "") {
+        document.getElementById("no-mensaje").style.display = "block";
+        document.getElementById("no-mensaje").textContent = "ATENCION: No se ha encontrado ningun texto a procesar, porfavor ingrese un mensaje en minisculas y sin caracteres especiales!";
+        document.getElementById("resultado").value = "";
+        return;
+    }
+  
+    //Logica de entrada, solo se permiten letras minúsculas sin acentos o caracteres especiales
+    if (/[^a-z]/.test(entrada)) {
+      document.getElementById("no-mensaje").style.display = "block";
+      document.getElementById("no-mensaje").textContent =
+        "ATENCION: Solo se permiten letras minúsculas sin acentos y sin caracteres especiales!";
+      document.getElementById("resultado").value = "";
+      return;
+    }
+    
+    
+    //Logica de encriptado, patron de remplazo
+    resultado = entrada
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+
+    //Salida del encriptado
+    document.getElementById("resultado").value = resultado;
+    document.getElementById("no-mensaje").style.display = "none";
+
 }
