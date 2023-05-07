2f3eb308ff6957711f3a900166ea623785f88bc0
Juan Basanes Cantoni <107370338+Juanbasanes@users.noreply.github.com>
2023-05-07 09:24:37 +0800

    encriptar y solo minúsculas

--- script.js
+++ script.js
@@ -8,21 +8,5 @@ function encriptar() {
       alert("El texto debe estar compuesto solo por letras minúsculas y sin acentos");
       return;
     }
-  
-    // Realizar la encriptación del texto
-    let resultado = "";
-    for (let i = 0; i < texto.length; i++) {
-      let charCode = texto.charCodeAt(i);
-      // La letra 'a' se convierte en la letra 'z'
-      if (charCode === 97) {
-        resultado += String.fromCharCode(122);
-      } else {
-        // Las demás letras se convierten en la letra anterior
-        resultado += String.fromCharCode(charCode - 1);
-      }
-    }
-  
-    // Mostrar el resultado de la encriptación en la pantalla
-    document.getElementById("resultado").value = resultado;
-  }
+
   
